601f5b309a9ddeef082f75204564fb436742983e
Andrew Holford <andrewholford@gmail.com>
2020-06-02 05:57:13 +0100

    rename tf file

--- prod.tf
+++ prod.tf
@@ -4,6 +4,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "first_bucket" {
-  bucket = "drofloh_first_bucket"
+  bucket = "droflohfirstbucket"
   acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {}
