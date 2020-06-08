579affe6f0e84f60b0f91036f06477fcd12edc5c
Andrew Holford <andrewholford@gmail.com>
2020-06-08 05:35:40 +0100

    add output for elb dns name

--- prod.tf
+++ prod.tf
@@ -101,21 +101,22 @@ resource "aws_elb" "prod_web" {
 }
 
 resource "aws_launch_template" "prod_web" {
-  name_prefix   = "prod_web"
-  image_id      = var.web_image_id
-  instance_type = var.web_instance_type
+  name_prefix            = "prod_web"
+  image_id               = var.web_image_id
+  instance_type          = var.web_instance_type
   vpc_security_group_ids = [aws_security_group.prod_web.id]
+
   tags = {
     "Terraform" : "true"
   }
 }
 
 resource "aws_autoscaling_group" "prod_web" {
-  availability_zones = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  availability_zones  = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id, aws_default_subnet.default_az3.id]
-  desired_capacity   = var.web_desired_capacity
-  max_size           = var.web_max_size
-  min_size           = var.web_min_size
+  desired_capacity    = var.web_desired_capacity
+  max_size            = var.web_max_size
+  min_size            = var.web_min_size
 
   launch_template {
     id      = aws_launch_template.prod_web.id
@@ -132,4 +133,8 @@ resource "aws_autoscaling_group" "prod_web" {
 resource "aws_autoscaling_attachment" "prod_web" {
   autoscaling_group_name = aws_autoscaling_group.prod_web.id
   elb                    = aws_elb.prod_web.id
+}
+
+output "dns_name" {
+  value = aws_elb.prod_web.dns_name
 }
